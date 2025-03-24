2ab9de95ae7f529a1c93f1d8ead28f19b811ce4d
sam83biswas <sam83biswas@gmail.com>
2025-03-25 02:35:32 +0530

    Create Solution.java

---
+++ Pattern/Triangle Pattern1/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public static void trnglPtrn(int n){
+        
+        for(int i = 0; i<n; i++){
+            
+            for(int j = 0;j<i+1;j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        trnglPtrn(3);
+    }
+}
