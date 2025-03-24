a61ff5268bc15e325fe91ad615ceb57a5a7c12d0
sam83biswas <sam83biswas@gmail.com>
2025-03-24 23:55:16 +0530

    Create Solution.java

---
+++ Maths/LCM/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public static int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    
+    public static int lcm(int a, int b){
+        return a*b/gcd(a,b);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(lcm(6,14));
+    }
+}
