5ec849df884952c1f0e554fdd7f68c3c4e5985ec
sam83biswas <sam83biswas@gmail.com>
2025-03-25 01:52:28 +0530

    Update Solution.java

--- Pattern/Squre Pattern/Solution.java
+++ Pattern/Squre Pattern/Solution.java
@@ -1,9 +1,11 @@
-class Main {
+class Solution {
     
     public static void sqrPtrn(int n){
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n; j++){
-                System.out.print(j + " ");
+        for(int i = 0; i<n; i++){
+            char ch = 'A';
+            for(int j = 0; j<n; j++){
+                System.out.print(ch + " ");
+                ch += 1;
             }
             System.out.println();
         }
