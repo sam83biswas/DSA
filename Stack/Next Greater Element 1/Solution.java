d12a87e41e1d2159e0d6f5cab052927bfdd5f0e2
sam83biswas <sam83biswas@gmail.com>
2025-04-06 12:50:46 +0530

    Update Solution.java

--- Stack/Next Greater Element 1/Solution.java
+++ Stack/Next Greater Element 1/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int findElement(int elmnt,int[] arr){
-        for(int i = 0;i<arr.length;i++){
-            if(elmnt == arr[i]){
-                return i;
-            }
-        }
-        return -1;
-
-    }
-    public int nxtGrtr(int idx, int[] nums2){
-        for(int i = idx+1; i<nums2.length; i++){
-            if(nums2[idx]<nums2[i]){
-                return nums2[i];
-            }
-        }
-        return -1;
-    }
+    
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Stack<Integer> stk = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int[] ans = new int[nums1.length];
-        for(int i = 0;i<nums1.length;i++){
-            int idx = findElement(nums1[i], nums2); 
-            ans[i] = nxtGrtr(idx, nums2);
+
+        for(int i = nums2.length-1; i>=0; i--){
+            while( !stk.isEmpty() && nums2[i]>=nums2[stk.peek()]){
+                stk.pop();
+            }
+            if(stk.isEmpty()){
+                map.put(nums2[i], -1);
+            }else{
+                map.put(nums2[i],nums2[stk.peek()]);
+            }
+            stk.push(i);
         }
+
+        for(int i = 0;i<nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
+        }
+
         return ans;
-        
+
     }
 }
