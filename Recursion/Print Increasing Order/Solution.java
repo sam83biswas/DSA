3d5d98bf99ef964c5f8e71a324e226d3a74fe43c
sam83biswas <sam83biswas@gmail.com>
2025-04-15 00:27:20 +0530

    Create Solution.java

---
+++ Recursion/Print Increasing Order/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static void print(int num){
+        if(num == 0){
+            return;
+        }
+        print(num-1);
+        System.out.print(num + " ");
+    }
+    public static void main(String[] args) {
+        print(10);
+    }
+}
