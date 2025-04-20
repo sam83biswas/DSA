28414b700931dc8ed599bd953d336c670c7915f8
sam83biswas <sam83biswas@gmail.com>
2025-04-20 09:09:05 +0530

    Create Solution.java

---
+++ Binary Tree/Count Nodes/Solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    static class BinaryTree{
+        
+        public static int count(Node root){
+            if(root == null){
+                return 0;
+            }
+            int lc = count(root.left);
+            int rc = count(root.right);
+            return lc+rc+1;
+        }
+        
+        
+    }
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        
+        System.out.print(BinaryTree.count(root));
+        
+        
+    }
+}
