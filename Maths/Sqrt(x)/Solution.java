9bd910f7c83979ad4196fe113ebc9af6e7895893
sam83biswas <sam83biswas@gmail.com>
2025-05-15 21:01:26 +0530

    Create Solution.java

---
+++ Maths/Sqrt(x)/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        long left = 1;
+        long right = x;
+
+        while(left <= right){
+            long mid = left + (right-left)/2;
+
+            if(mid*mid == x){
+                return (int)mid;
+            }else if(mid*mid < x){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return (int)right;
+
+    }
+}
